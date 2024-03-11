1102fbfc051816cc3d3e700633d5fe709e7293d9
MrTipson <gunfight7.gf@gmail.com>
2024-03-11 15:51:43 +0100

    Fixate controls at the bottom of the screen and add background

--- src/components/Machine.tsx
+++ src/components/Machine.tsx
@@ -11,6 +11,7 @@ import {
 	ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { Toaster } from "@/components/ui/toaster";
+import { Separator } from "./ui/separator";
 
 export default function Machine() {
 	// set machine is called *only* when a new program is loaded, but will be mutated while stepping
@@ -33,7 +34,7 @@ export default function Machine() {
 						<ResizablePanel defaultSize={55}><HeapView machine={machine} className="h-full" step={step} /></ResizablePanel>
 						<ResizableHandle withHandle />
 						<ResizablePanel defaultSize={15}><StackView machine={machine} className="h-full" /></ResizablePanel>
-						<Controls className="absolute bottom-6 left-0 right-0 m-auto w-fit" machine={machine} setStep={setStep} />
+						<Controls className="absolute left-0 right-0 m-auto bottom-0 bg-background z-10 p-2 w-fit rounded-t border" machine={machine} setStep={setStep} />
 					</>
 					|| // not loaded
 					<>
