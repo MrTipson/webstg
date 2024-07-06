cede5a59111a4623eb6532ddbfb93976345125f5
MrTipson <gunfight7.gf@gmail.com>
2024-07-06 19:42:12 +0200

    Add mdx plugin and initial implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		react(),
+		mdx(),
 		tailwind({
 			applyBaseStyles: false,
 		})
---
+++ src/components/MdMachine.tsx
@@ -0,0 +1,70 @@
+import type { PropsWithChildren } from "react";
+import { type STGSettings } from "./Machine";
+import { build_ast } from "@/stglang/ASTBuilder";
+import { stg_machine } from "@/stgmachine/machine";
+import React from "react";
+import { classHighlighter, highlightCode } from "@lezer/highlight";
+import { parser } from "@/stglang/parser";
+import HeapView from "./HeapView";
+import StackView from "./StackView";
+
+const heap_slot = `<slot id="HeapView"></slot>`;
+const stack_slot = `<slot id="StackView"></slot>`;
+
+export function MdMachine({ children, program, step, garbage_collection, eval_apply, collapse_indirections }:
+	PropsWithChildren & STGSettings & { program: string, step: number }) {
+	const ast = build_ast(program);
+	const machine = new stg_machine(ast, eval_apply = eval_apply, garbage_collection = garbage_collection);
+	const settings: STGSettings = {
+		garbage_collection: garbage_collection,
+		eval_apply: eval_apply,
+		collapse_indirections: collapse_indirections
+	}
+	while (step > machine.step_number) {
+		machine.step();
+	}
+	const str = (children as React.ReactElement).props.value;
+	// Replace placeholder tags with the actual HeapView/StackView components
+	let index1 = str.indexOf(heap_slot);
+	let [element1, element2]: (JSX.Element | string)[] = ['', ''];
+	let [len1, len2] = [0, 0];
+	if (index1 !== -1) {
+		element1 = <HeapView className="h-[500px]" machine={machine} step={step} settings={settings} />
+		len1 = heap_slot.length;
+	}
+	let index2 = str.indexOf(stack_slot);
+	if (index2 !== -1) {
+		element2 = <StackView className="h-fit" machine={machine} />
+		len2 = stack_slot.length;
+	}
+	if (index2 < index1) {
+		[index1, index2] = [index2, index1];
+		[element1, element2] = [element2, element1];
+		[len1, len2] = [len2, len1];
+	}
+	const before = <div dangerouslySetInnerHTML={{ __html: str.substring(0, index1) }} />;
+	const between = <div dangerouslySetInnerHTML={{ __html: str.substring(index1 + len1, index2) }} />;
+	const after = <div dangerouslySetInnerHTML={{ __html: str.substring(index2 + len2) }} />;
+	return (
+		<>
+			{...[before, element1, between, element2, after]}
+		</>
+	);
+}
+
+export function Highlight({ program }: { program: string }) {
+	let children: any[] = [];
+
+	function emit(text: string, classes: string) {
+		children.push(React.createElement("span", { className: classes }, text));
+	}
+	function emitBreak() {
+		children.push(React.createElement("span", undefined, "\n"));
+	}
+	highlightCode(program, parser.parse(program), classHighlighter, emit, emitBreak);
+	return (
+		<pre>
+			<code>{...children}</code>
+		</pre>
+	);
+}
