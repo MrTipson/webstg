9e66c45be65ea590a21b4555ddbd0e685751b5bb
MrTipson <gunfight7.gf@gmail.com>
2024-07-10 01:13:05 +0200

    Markdown: update directory file handling

--- src/content/directory.ts
+++ src/content/directory.ts
@@ -1,15 +1,28 @@
+import config from "astro.config.mjs";
+const base = config.base || '';
+
 export type SubDirectory = { items: SidebarEntry[], collapsed?: boolean }
 export type Link = { link: string }
-export type Post = { slug: string }
-export type SidebarEntry = { label: string } & (SubDirectory | Link | Post)
+export type SidebarEntry = { label: string } & (SubDirectory | Link)
 
-export const sidebar: SidebarEntry[] = [
+export function loadDirectory(entries: SidebarEntry[]): SidebarEntry[] {
+	return entries.map((entry) => {
+		if ("link" in entry) {
+			const link = entry.link.startsWith('/') ? base + entry.link : entry.link;
+			return { label: entry.label, link: link };
+		} else {
+			return { label: entry.label, items: loadDirectory(entry.items) };
+		}
+	});
+}
+
+export const sidebar: SidebarEntry[] = loadDirectory([
 	{
 		label: 'My markdown files',
 		items: [
-			{ label: 'Markdown test', slug: 'markdown' },
-			{ label: 'Other file', slug: 'other' },
+			{ label: 'Markdown test', link: '/docs/markdown' },
+			{ label: 'Other file', link: '/docs/other' },
 		]
 	},
-	{ label: 'WebSTG sandbox', link: '/webstg/live' },
-]
+	{ label: 'WebSTG sandbox', link: '/live' },
+]);
