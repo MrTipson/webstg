8e2a24f37983eeef2df2d4cc39b370ae0e79b3e8
MrTipson <gunfight7.gf@gmail.com>
2024-08-11 12:32:24 +0200

    Syntax highlighting: highlight line comments and data keyword

--- src/stglang/highlight.js
+++ src/stglang/highlight.js
@@ -2,7 +2,7 @@ import { styleTags, tags } from "@lezer/highlight"
 
 export const stgHighlight = styleTags({
 	"FUN THUNK CON PAP": tags.typeName,
-	"let letrec in case of": tags.controlKeyword,
+	"let letrec in case of data": tags.controlKeyword,
 	Literal: tags.number,
 	Identifier: tags.variableName,
 	Binding: tags.definition(tags.variableName),
